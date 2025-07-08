ea934d266ef591786801cac8c49f8045cf9acc31
Raam Ahmadi <ahmadiramin0@gmail.com>
2025-07-08 21:52:53 +0330

    delete backslash

--- lib/api.ts
+++ lib/api.ts
@@ -18,7 +18,7 @@ export async function fetchAPI(query: string, variables = {}) {
   
 // REST API fetch function
 export async function fetchRestAPI(endpoint: string, options: RequestInit = {}) {
-  const url = `${restApiUrl}/${endpoint}`;
+  const url = `${restApiUrl}${endpoint}`;
   
   try {
     console.log(`Fetching from REST API: ${url}`);
